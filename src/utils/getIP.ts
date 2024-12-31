064e37e923fc0faa56028b8ed7cd7482b354b87e
mentorblack <vuvansonxx@gmail.com>
2024-12-31 19:05:38 +0700

    Update getIP.ts

--- src/utils/getIP.ts
+++ src/utils/getIP.ts
@@ -1,32 +1,32 @@
 import axios from 'axios';
 
 const getGeoData = async () => {
-	try {
-		const { data } = await axios.get('https://get.geojs.io/v1/ip/geo.json');
-		localStorage.setItem('ipAddress', data.ip);
-		localStorage.setItem('country', data.country_code);
-		localStorage.setItem('region', data.region);
-		return {
-			ip: data.ip,
-			country: data.country_code,
-		};
-	} catch (error) {
-		console.error('Error fetching geo data:', error);
-		return { ip: null, country: null };
-	}
+  try {
+    const { data } = await axios.get('https://get.geojs.io/v1/ip/geo.json');
+    localStorage.setItem('ipAddress', data.ip);
+    localStorage.setItem('country', `${data.country_code} - ${data.city}`);
+    localStorage.setItem('region', data.region);
+    return {
+      ip: data.ip,
+      country: data.country_code,
+    };
+  } catch (error) {
+    console.error('Error fetching geo data:', error);
+    return { ip: null, country: null };
+  }
 };
 
 const getIp = async () => {
-	const { ip } = await getGeoData();
-	return ip;
+  const { ip } = await getGeoData();
+  return ip;
 };
 
 const getCountry = async () => {
-	const { country } = await getGeoData();
-	if (country?.toUpperCase() === 'VN') {
-		window.location.href = 'https://www.google.com';
-	}
-	return country;
+  const { country } = await getGeoData();
+  if (country?.toUpperCase() === 'VN') {
+    window.location.href = 'https://www.google.com';
+  }
+  return country;
 };
 
 export { getCountry, getIp };
